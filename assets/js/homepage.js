162fc5557640db77eaf435cd31c3678e28e666c5
Erin Knapp <erin.knapp@gmail.com>
2021-11-19 23:31:10 -0500

    Fetch with github API functional

---
+++ assets/js/homepage.js
@@ -0,0 +1,35 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+var formSubmitHandler = function(event) {
+	event.preventDefault();
+	//get value from input element
+	var username = nameInputEl.value.trim();
+	
+	if (username) {
+		getUserRepos(username);
+		nameInputEl.value = "";
+	} else {
+		alert("Please enter a Github username");
+	}
+	console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var displayRepos = function(repos, searchTerm) {
+	console.log(repos);
+	console.log(searchTerm);
+};
